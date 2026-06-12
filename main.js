10548ecfd7c0e421133ec7a1dcd6b7ee93335f1d
DiegoBrancolij <brancoli.99@gmail.com>
2026-06-12 17:24:13 -0400

    agregar carpeta assets

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // El DOM se ha cargado. Aquí puedes agregar tu lógica de JavaScript en el futuro.
+    console.log("Portfolio inicializado correctamente.");
+});
